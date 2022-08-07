b9b1c8613165ee678863e0e44464b4fec45cd9f1
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-07 19:58:19 -0300

    Adicionando label pagina

--- src/Components/Form-CadastroLivro/index.js
+++ src/Components/Form-CadastroLivro/index.js
@@ -36,62 +36,64 @@ function Index() {
     }
 
   return (
-    <div id='AreaForm-CadastroLivro'>
-        <Form>
-            
-            <div>
-                <Form.Group className='Form-group'>
-                    <Form.Label>TÍTULO</Form.Label>
-                    <Form.Control className='Form-control' type="text" required 
-                    onChange={e => setTitle(e.target.value)} />
-                </Form.Group>
-                <Form.Group className='Form-group'>
-                    <Form.Label>EDIÇÃO/VOLUME</Form.Label>
-                    <Form.Control className='Form-control' type="text" required
-                    onChange={e => setEdition(e.target.value)} />
-                </Form.Group>
-                <Form.Group className='Form-group'>
-                    <Form.Label>CATEGORIA</Form.Label>
-                    <Form.Control className='Form-control' type="text" required
-                    onChange={e => setCategory(e.target.value)}/>
-                </Form.Group>
-            </div>
+    <div id="mainCadastroLivro">
+        <label id="Label-CadastroLivro">CADASTRO DE LIVRO</label>
+        <div id='AreaForm-CadastroLivro'>
+            <Form>
+                <div>
+                    <Form.Group className='Form-group'>
+                        <Form.Label>TÍTULO</Form.Label>
+                        <Form.Control className='Form-control' type="text" required 
+                        onChange={e => setTitle(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className='Form-group'>
+                        <Form.Label>EDIÇÃO/VOLUME</Form.Label>
+                        <Form.Control className='Form-control' type="text" required
+                        onChange={e => setEdition(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className='Form-group'>
+                        <Form.Label>CATEGORIA</Form.Label>
+                        <Form.Control className='Form-control' type="text" required
+                        onChange={e => setCategory(e.target.value)}/>
+                    </Form.Group>
+                </div>
 
-            <div>
-                <Form.Group className='Form-group'>
-                    <Form.Label>IDIOMA</Form.Label>
-                    <Form.Control className='Form-control' type="text" required
-                    onChange={e => setIdiom(e.target.value)} />
-                </Form.Group>
-                <Form.Group className='Form-group'>
-                    <Form.Label>ANO LANÇAMENTO</Form.Label>
-                    <Form.Control className='Form-control' type="number" maxLength={4} required
-                    onChange={e => setYear(e.target.value)} />
-                </Form.Group>
-                <Form.Group className='Form-group'>
-                    <Form.Label>AUTOR</Form.Label>
-                    <Form.Control className='Form-control' type="text" required
-                    onChange={e => setAuthor(e.target.value)}/>
-                </Form.Group>
-            </div>
+                <div>
+                    <Form.Group className='Form-group'>
+                        <Form.Label>IDIOMA</Form.Label>
+                        <Form.Control className='Form-control' type="text" required
+                        onChange={e => setIdiom(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className='Form-group'>
+                        <Form.Label>ANO LANÇAMENTO</Form.Label>
+                        <Form.Control className='Form-control' type="number" maxLength={4} required
+                        onChange={e => setYear(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group className='Form-group'>
+                        <Form.Label>AUTOR</Form.Label>
+                        <Form.Control className='Form-control' type="text" required
+                        onChange={e => setAuthor(e.target.value)}/>
+                    </Form.Group>
+                </div>
 
-            <div>
-                <Form.Group className='Form-group'>
-                    <Form.Label>ISBN</Form.Label>
-                    <Form.Control className='Form-control' type="text" required
-                    onChange={e => setIsbn(e.target.value)}/>
-                </Form.Group>
-                <Form.Group className='Form-group'>
-                    <Form.Label>CDD</Form.Label>
-                    <Form.Control className='Form-control' type="text" required
-                    onChange={e => setCdd(e.target.value)}/>
-                </Form.Group>
-            </div>
+                <div>
+                    <Form.Group className='Form-group'>
+                        <Form.Label>ISBN</Form.Label>
+                        <Form.Control className='Form-control' type="text" required
+                        onChange={e => setIsbn(e.target.value)}/>
+                    </Form.Group>
+                    <Form.Group className='Form-group'>
+                        <Form.Label>CDD</Form.Label>
+                        <Form.Control className='Form-control' type="text" required
+                        onChange={e => setCdd(e.target.value)}/>
+                    </Form.Group>
+                </div>
 
-            <div id="AreaButton-CadastrarLivro">
-            <Button id="FormButton-CadastrarLivro" type="submit" onClick={sendBook}>ENVIAR</Button>
-            </div>
-        </Form>
+                <div id="AreaButton-CadastrarLivro">
+                <Button id="FormButton-CadastrarLivro" type="submit" onClick={sendBook}>ENVIAR</Button>
+                </div>
+            </Form>
+        </div>
     </div>
   )
 }
--- src/Pages/CadastroLivro/index.js
+++ src/Pages/CadastroLivro/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import FormCadastroLivro from '../../Components/Form-CadastroLivro';
-import './index.css'
 
 function index() {
   return (
     <div>
-      {/* <label id='Label-CadastrarLivro'>CADASTRO DE LIVRO</label> */}
       <FormCadastroLivro />
     </div>
   )
