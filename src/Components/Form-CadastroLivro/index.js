52e42ba566094c584dc0346c54e59e4b2246a84d
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-08 14:14:06 -0300

    Posicionamento label && inputs

--- src/Components/Form-CadastroLivro/index.js
+++ src/Components/Form-CadastroLivro/index.js
@@ -43,52 +43,59 @@ function Index() {
         <div id='AreaForm-CadastroLivro'>
             <Form>
                 <div id="AreaFormGroup">
-                    <Form.Group className='Form-group'>
-                        <Form.Label>TÍTULO</Form.Label>
-                        <Form.Control className='Form-control' type="text" required 
-                        onChange={e => setTitle(e.target.value)} />
-                    </Form.Group>
-                    <Form.Group className='Form-group'>
-                        <Form.Label>EDIÇÃO/VOLUME</Form.Label>
-                        <Form.Control className='Form-control' type="text" required
-                        onChange={e => setEdition(e.target.value)} />
-                    </Form.Group>
-                    <Form.Group className='Form-group'>
-                        <Form.Label>CATEGORIA</Form.Label>
-                        <Form.Control className='Form-control' type="text" required
-                        onChange={e => setCategory(e.target.value)}/>
-                    </Form.Group>
+                    <div>
+                        <Form.Group className='Form-group'>
+                            <Form.Label>TÍTULO</Form.Label>
+                            <Form.Control className='Form-control' type="text" required 
+                            onChange={e => setTitle(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className='Form-group'>
+                            <Form.Label>EDIÇÃO/VOLUME</Form.Label>
+                            <Form.Control className='Form-control' type="text" required
+                            onChange={e => setEdition(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className='Form-group'>
+                            <Form.Label>CATEGORIA</Form.Label>
+                            <Form.Control className='Form-control' type="text" required
+                            onChange={e => setCategory(e.target.value)}/>
+                        </Form.Group>
+                    </div>
 
-                    <Form.Group className='Form-group'>
-                        <Form.Label>IDIOMA</Form.Label>
-                        <Form.Control className='Form-control' type="text" required
-                        onChange={e => setIdiom(e.target.value)} />
-                    </Form.Group>
-                    <Form.Group className='Form-group'>
-                        <Form.Label>ANO LANÇAMENTO</Form.Label>
-                        <Form.Control className='Form-control' type="number" maxLength={4} required
-                        onChange={e => setYear(e.target.value)} />
-                    </Form.Group>
-                    <Form.Group className='Form-group'>
-                        <Form.Label>AUTOR</Form.Label>
-                        <Form.Control className='Form-control' type="text" required
-                        onChange={e => setAuthor(e.target.value)}/>
-                    </Form.Group>
+                    <div>
+                        <Form.Group className='Form-group'>
+                            <Form.Label>IDIOMA</Form.Label>
+                            <Form.Control className='Form-control' type="text" required
+                            onChange={e => setIdiom(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className='Form-group'>
+                            <Form.Label>ANO LANÇAMENTO</Form.Label>
+                            <Form.Control className='Form-control' type="number" maxLength={4} required
+                            onChange={e => setYear(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group className='Form-group'>
+                            <Form.Label>AUTOR</Form.Label>
+                            <Form.Control className='Form-control' type="text" required
+                            onChange={e => setAuthor(e.target.value)}/>
+                        </Form.Group>
+                    </div>
 
-                    <Form.Group className='Form-group'>
-                        <Form.Label>ISBN</Form.Label>
-                        <Form.Control className='Form-control' type="text" required
-                        onChange={e => setIsbn(e.target.value)}/>
-                    </Form.Group>
-                    <Form.Group className='Form-group'>
-                        <Form.Label>CDD</Form.Label>
-                        <Form.Control className='Form-control' type="text" required
-                        onChange={e => setCdd(e.target.value)}/>
-                    </Form.Group>
-                </div>
 
-                <div id="AreaButton-CadastrarLivro">
-                <Button id="FormButton-CadastrarLivro" type="submit">ENVIAR</Button>
+                    <div>
+                        <Form.Group className='Form-group'>
+                            <Form.Label>ISBN</Form.Label>
+                            <Form.Control className='Form-control' type="text" required
+                            onChange={e => setIsbn(e.target.value)}/>
+                        </Form.Group>
+                        <Form.Group className='Form-group'>
+                            <Form.Label>CDD</Form.Label>
+                            <Form.Control className='Form-control' type="text" required
+                            onChange={e => setCdd(e.target.value)}/>
+                        </Form.Group>
+                    </div>
+
+                    <div id="AreaButton-CadastrarLivro">
+                        <Button id="FormButton-CadastrarLivro" type="submit">ENVIAR</Button>
+                    </div>
                 </div>
             </Form>
         </div>
