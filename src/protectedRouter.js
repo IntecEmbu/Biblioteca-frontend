9157e266f75808263f7bfe93b3c4c11f3ce618f3
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-31 22:25:26 -0300

    Delete

--- src/protectedRouter.js
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-function protectedRouter(props){
-  return props.isAuth ? <Outlet/> : <Navigate to='/Login'/> 
-}
-
-export default protectedRouter;
