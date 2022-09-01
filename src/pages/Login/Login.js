9419514f16ac90b7cac1691be457dd77a77efcd1
pedrofnseca <pedropereira.pp264@gmail.com>
2022-08-31 22:33:17 -0300

    Subindo site

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from '../../components/Login/Login.js'
+import Footer from '../../components/Footer/Footer.js'
+
+function index() {
+  return (
+    <>
+      <Login />
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default index
