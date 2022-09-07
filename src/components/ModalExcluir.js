73896df420320f82280d999df1487c9c22702fe7
pedrofnseca <pedropereira.pp264@gmail.com>
2022-09-07 14:33:15 -0300

    Limitando botão de exclusão

--- src/components/ModalExcluir.js
+++ src/components/ModalExcluir.js
@@ -1,14 +1,37 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react'
 import Modal from 'react-bootstrap/Modal';
 import { FaTrashAlt, FaPen } from "react-icons/fa"
+import api from '../service/api'
 import '../styles/Modal.css'
+import Spinner from 'react-bootstrap/Spinner'
 
-function Example() {
+function Example(props) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [isDisabled, setIsDisabled] = useState(false)
+  const [spinner, setSpinner] = useState('')
+
+  const way = props.way
+
+  async function deleteitem(){
+    console.log(way)
+    try{
+      setSpinner(<Spinner id="loading" animation='border' />)
+      setIsDisabled(true)
+
+      await api.delete(`${way}`)
+      alert('Excluido com sucesso!')
+      window.location.reload()
+    } catch(err){
+      alert('Erro ao excluir!')
+      setSpinner('')
+      setIsDisabled(false)
+      console.log(err)
+    }
+  }
 
   return (
     <>
@@ -21,12 +44,21 @@ function Example() {
         <Modal.Header>
           <Modal.Title>Excluir</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Tem certeza que deseja excluir?</Modal.Body>
+        <Modal.Body>
+          Tem certeza que deseja excluir?
+          <div className="spinner-login">
+            {spinner}
+          </div>
+        </Modal.Body>
         <Modal.Footer>
-          <button className="btn-cancelar-modal" onClick={handleClose}>
+          <button className="btn-cancelar-modal" 
+          onClick={handleClose}
+          disabled={isDisabled}>
             Cancelar
           </button>
-          <button className="btn-excluir-modal">
+          <button className="btn-excluir-modal" 
+          onClick={deleteitem}
+          disabled={isDisabled}>
             <FaTrashAlt className="fa-trash" />
             Excluir
           </button>
