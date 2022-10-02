d4f100868ab8bcd90ce2bafe612403cd210a7b12
pedrofnseca <pedropereira.pp264@gmail.com>
2022-10-02 19:03:12 -0300

    Válidação de campos

--- src/components/ModalEditarAluno.js
+++ src/components/ModalEditarAluno.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
 import { FaPen } from "react-icons/fa";
 import api from "../service/api";
-import Spinner from "react-bootstrap/Spinner";
+import { Spinner } from "react-bootstrap";
 
 function ModalEditarAluno({ data }) {
   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setErrors({});
+    setShow(false);
+  };
   const handleShow = () => setShow(true);
 
   const id = data.id;
@@ -18,34 +21,97 @@ function ModalEditarAluno({ data }) {
   const [type, setType] = useState(data.type);
   const [cpf, setCpf] = useState(data.cpf);
 
-  const [spinner, setSpinner] = useState("");
+  const [errors, setErrors] = useState({});
+  const [success, setSuccess] = useState(false);
+
   const [isDisabled, setIsDisabled] = useState(false);
 
-  async function updateUser() {
-    setSpinner(<Spinner id="loading" animation="border" />);
-    setIsDisabled(true);
+  async function close() {
+    setInterval(() => {
+      setIsDisabled(false);
+      handleClose();
+      window.location.reload();
+    }, 2000);
+  }
 
-    try {
-      await api.put("/user/update-user", {
-        id,
-        name,
-        email,
-        phone,
-        course,
-        type,
-        cpf,
-      });
-      alert("Aluno atualizado com sucesso!");
-    } catch (err) {
-      alert("Erro ao atualizar aluno!");
-      console.log(err);
+  function validate() {
+    let errors = {};
+    let count = 0;
+
+    // Validação do nome
+    if (!name) {
+      errors.name = "Campo obrigatório";
+      count++;
+    } else if (name.length > 100) {
+      errors.name = "Nome muito longo";
+      count++;
     }
 
-    setSpinner("");
-    setIsDisabled(false);
+    // Validação do cpf
+    if (!cpf) {
+      errors.cpf = "Campo obrigatório";
+      count++;
+    } else if (cpf.length > 11 || cpf.length < 11) {
+      errors.cpf = "CPF inválido";
+      count++;
+    }
 
-    handleClose();
-    window.location.reload();
+    // Validação do email
+    if (!email) {
+      errors.email = "Campo obrigatório";
+      count++;
+    }
+
+    // Validação do telefone
+    if (!phone) {
+      errors.phone = "Campo obrigatório";
+      count++;
+    }
+
+    if (count > 0) {
+      errors.count = `Existem ${count} campos preenchidos incorretamente`;
+    }
+
+    // Casso não haja erros, o objeto errors estará vazio e irá retornar true
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors);
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  async function updateUser() {
+    if (validate()) {
+      setIsDisabled(true);
+      try {
+        await api.put("/user/update-user", {
+          id,
+          name,
+          email,
+          phone,
+          course,
+          type,
+          cpf,
+        });
+        setSuccess(true);
+        setIsDisabled(false);
+      } catch (err) {
+        alert("Erro ao cadastrar o aluno!");
+        console.log(err);
+        setIsDisabled(false);
+      } finally {
+        await close();
+      }
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  function handleKeyDown(event) {
+    if (event.keyCode === 13) {
+      updateUser();
+    }
   }
 
   return (
@@ -65,51 +131,88 @@ function ModalEditarAluno({ data }) {
               <div className="input-box-modal">
                 <label>Nome</label>
                 <input
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
                   type="text"
-                  required
+                  value={name}
+                  onChange={(e) => {
+                    setName(e.target.value);
+                    setErrors({ ...errors, name: "", count: "" });
+                  }}
+                  onKeyDown={handleKeyDown}
                 />
+                {errors.name && <p className="error-message">{errors.name}</p>}
               </div>
 
               <div className="input-box-modal">
                 <label>Curso</label>
-                <input
-                  value={course}
-                  onChange={(e) => setCourse(e.target.value)}
-                  type="text"
-                  required
-                />
+                <select
+                  defaultValue={course}
+                  onChange={(e) => {
+                    setCourse(e.target.value);
+                    console.log(e.target.value);
+                  }}
+                >
+                  <option value="Novotec Administração">
+                    Novotec Administração
+                  </option>
+                  <option value="Novotec Automação Industrial">
+                    Novotec Automação Industrial
+                  </option>
+                  <option value="Administração">Administração</option>
+                  <option value="Contabilidade">Contabilidade</option>
+                  <option value="Desenvolvimento de Sistemas">
+                    Desenvolvimento de Sistemas
+                  </option>
+                  <option value="Redes de Computadores">
+                    Redes de Computadores
+                  </option>
+                  <option value="Eletroeletrônica">Eletroeletrônica</option>
+                  <option value="Logística">Logística</option>
+                </select>
               </div>
 
               <div className="input-box-modal">
                 <label>CPF</label>
                 <input
                   type="text"
-                  required
-                  onchange={(e) => setCpf(e.target.value)}
                   value={cpf}
+                  onChange={(e) => {
+                    setCpf(e.target.value);
+                    setErrors({ ...errors, cpf: "", count: "" });
+                  }}
+                  onKeyDown={handleKeyDown}
                 />
+                {errors.cpf && <p className="error-message">{errors.cpf}</p>}
               </div>
 
               <div className="input-box-modal">
                 <label>E-mail</label>
                 <input
                   value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    setErrors({ ...errors, email: "", count: "" });
+                  }}
                   type="email"
-                  required
                 />
+                {errors.email && (
+                  <p className="error-message">{errors.email}</p>
+                )}
               </div>
 
               <div className="input-box-modal">
                 <label>Celular</label>
                 <input
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
                   type="text"
-                  required
+                  value={phone}
+                  onChange={(e) => {
+                    setPhone(e.target.value);
+                    setErrors({ ...errors, phone: "", count: "" });
+                  }}
+                  onKeyDown={handleKeyDown}
                 />
+                {errors.phone && (
+                  <p className="error-message">{errors.phone}</p>
+                )}
               </div>
 
               <div className="input-box-modal">
@@ -125,8 +228,18 @@ function ModalEditarAluno({ data }) {
               </div>
             </div>
           </form>
+          {errors.count && (
+            <p className="error-count-message">{errors.count}</p>
+          )}
+          {isDisabled && (
+            <div className="loading-modal">
+              <Spinner animation="border" />
+            </div>
+          )}
+          {success && (
+            <p className="success-message">Aluno cadastrado com sucesso!</p>
+          )}
         </Modal.Body>
-        <div className="spinner-login">{spinner}</div>
         <Modal.Footer>
           <button
             className="btn-cancelar-modal"
