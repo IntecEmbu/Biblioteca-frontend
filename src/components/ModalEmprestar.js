9e9ee8b39a64ad174fcd112eeb2837f2234eb155
pedrofnseca <pedropereira.pp264@gmail.com>
2022-10-03 16:04:25 -0300

    Estrutura para emprestimo de livro

--- src/components/ModalEmprestar.js
+++ src/components/ModalEmprestar.js
@@ -7,7 +7,11 @@ import Spinner from "react-bootstrap/Spinner";
 function ModalEditarAluno({ data }) {
   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setErrors({});
+    setUserCpf("");
+    setShow(false);
+  };
   const handleShow = () => setShow(true);
 
   const id = data.id;
@@ -17,26 +21,78 @@ function ModalEditarAluno({ data }) {
   );
   const [userCpf, setUserCpf] = useState("");
 
-  const [spinner, setSpinner] = useState("");
+  const [errors, setErrors] = useState({});
+  const [success, setSuccess] = useState(false);
   const [isDisabled, setIsDisabled] = useState(false);
 
-  async function lending() {
-    setSpinner(<Spinner id="loading" animation="border" />);
-    setIsDisabled(true);
+  async function close() {
+    setInterval(() => {
+      setIsDisabled(false);
+      handleClose();
+      window.location.reload();
+    }, 2000);
+  }
 
-    try {
-      await api.put("/user/update-user");
-      alert("Livro emprestado!");
-    } catch (err) {
-      alert("Erro ao emprestar o livro!");
-      console.log(err);
+  function formatCpf(number) {
+    // Deixa o cpf no formato 000.000.000-00
+    return setUserCpf(
+      number.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+    );
+  }
+
+  async function simulaReaquisição() {
+    setInterval(() => {
+      console.log("simulando reaquisição");
+    }, 5000);
+  }
+
+  function validate() {
+    let errors = {};
+    let count = 0;
+
+    // Validação do cpf
+    if (!userCpf) {
+      errors.userCpf = "Campo obrigatório";
+      count++;
+    } else if (!userCpf.match(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)) {
+      errors.userCpf = "CPF inválido";
+      count++;
     }
 
-    setSpinner("");
-    setIsDisabled(false);
+    if (Object.keys(errors).length > 0) {
+      setErrors({ ...errors, count: count });
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-    handleClose();
-    window.location.reload();
+  async function lending() {
+    if (validate()) {
+      setIsDisabled(true);
+      try {
+        await api.post("/lending", {
+          book_id,
+          librarian_id,
+          userCpf,
+        });
+        setIsDisabled(false);
+        setSuccess(true);
+      } catch (err) {
+        // alert("Erro ao emprestar o livro!");
+        // console.log(err);
+        setIsDisabled(false); // Lembrar de tirar dps
+        setSuccess(true);
+      } finally {
+        await close();
+      }
+    }
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      lending();
+    }
   }
 
   return (
@@ -50,20 +106,34 @@ function ModalEditarAluno({ data }) {
           <Modal.Title>Emprestar Livro</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form>
-            <div className="input-group-modal">
-              <div className="input-box-modal">
-                <label>CPF</label>
-                <input
-                  type="text"
-                  required
-                  onChange={(e) => setUserCpf(e.target.value)}
-                />
-              </div>
+          <div className="input-group-modal">
+            <div className="input-box-modal">
+              <label>CPF</label>
+              <input
+                type="text"
+                value={userCpf}
+                onChange={(e) => {
+                  formatCpf(e.target.value);
+                  setErrors({ ...errors, userCpf: "", count: "" });
+                }}
+                onKeyDown={handleKeyDown}
+              />
+              {errors.userCpf && (
+                <span className="error-message">{errors.userCpf}</span>
+              )}
+              {success && (
+                <span className="success-message">
+                  Emprestimo realizado com sucesso!
+                </span>
+              )}
+              {isDisabled && (
+                <div className="loading-modal">
+                  <Spinner animation="border" />
+                </div>
+              )}
             </div>
-          </form>
+          </div>
         </Modal.Body>
-        <div className="spinner-login">{spinner}</div>
         <Modal.Footer>
           <button
             className="btn-cancelar-modal"
