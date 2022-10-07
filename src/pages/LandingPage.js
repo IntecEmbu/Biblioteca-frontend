a2fada33993d4c59488348226d1d5b2ff5d328a2
pedrofnseca <pedropereira.pp264@gmail.com>
2022-10-07 00:00:21 -0300

    Navbar landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,33 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/LandingPage.css";
 
 function LandingPage() {
   return (
-    <div id="landing-page">
-      <div className="podio-area">
-        <span className="podio-title">Leitores do mês</span>
-        <div className="podio-container">
-          <div className="podio">
-            <div className="podio-item">
-              <p className="podio-item-name">Pedro F.</p>
-              <div id="podio-item-3">3</div>
+    <>
+      <div className="navbar">
+        <div className="navbar-logo">
+          <img src={require("../images/logo.png")} alt="Logo" />
+        </div>
+        <div className="Navbar-link item-nav">
+          <Link to="/login" id="link-navbar" className="link-navbar-sair">
+            Entrar
+          </Link>
+        </div>
+      </div>
+
+      <div id="landing-page">
+        <div className="podio-area">
+          <span className="podio-title">Leitores do mês</span>
+          <div className="podio-container">
+            <div className="podio">
+              <div className="podio-item">
+                <p className="podio-item-name">Pedro F.</p>
+                <div id="podio-item-3">3</div>
+              </div>
             </div>
-          </div>
-          <div className="podio">
-            <div className="podio-item">
-              <p className="podio-item-name">Natan B.</p>
-              <div id="podio-item-1">1</div>
+            <div className="podio">
+              <div className="podio-item">
+                <p className="podio-item-name">Natan B.</p>
+                <div id="podio-item-1">1</div>
+              </div>
             </div>
-          </div>
-          <div className="podio">
-            <div className="podio-item">
-              <p className="podio-item-name">João F.</p>
-              <div id="podio-item-2">2</div>
+            <div className="podio">
+              <div className="podio-item">
+                <p className="podio-item-name">João F.</p>
+                <div id="podio-item-2">2</div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
