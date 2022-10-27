95a045e270da91ddd64ebb8bf3bc4f3195ff86bd
pedrofnseca <pedropereira.pp264@gmail.com>
2022-10-26 23:04:23 -0300

    Ajuste css e troca de api

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://biblitec-api.herokuapp.com",
+  baseURL: "https://biblitec.azurewebsites.net",
 });
 
 export default api;
