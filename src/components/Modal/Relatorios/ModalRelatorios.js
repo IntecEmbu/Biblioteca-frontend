ef839569cdc746472688532a6c700353d4e0c95b
pedrofnseca <pedropereira.pp264@gmail.com>
2022-11-20 23:23:02 -0300

    :sparkles: feat: gerador de xlsx de relatório

--- src/components/Modal/Relatorios/ModalRelatorios.js
+++ src/components/Modal/Relatorios/ModalRelatorios.js
@@ -1,18 +1,52 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import "../../../styles/Modal.css";
 import "../../../styles/Botoes.css";
+import api from "../../../service/api.js";
+import { Spinner } from 'react-bootstrap';
+import {utils, writeFile} from 'xlsx';
 
 function Index() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const [type, setType] = useState("inventory");
+  const [returned, setReturned] = useState(false);
+
+  async function download(){
+    setIsDisabled(true);
+    let response
+    let filename
+    const date = new Date().toLocaleDateString().split('/').join('-')
+
+    if(type === "lending"){
+      response = await api.get(`/report/generator/lending?returned=${returned}`)
+      filename = `emprestimos_${date}.xlsx`
+    } else if(type === "inventory"){
+      response = await api.get(`/report/generator/book`)
+      filename = `inventario.xlsx`
+    }
+
+    const wb = utils.book_new()
+    wb.Props = {
+      Title: filename,
+      author: "Biblioteca de Embu das Artes",
+      createdDate: date
+    }
+
+    wb.SheetNames.push(filename)
+    wb.Sheets[filename] = utils.json_to_sheet(response.data)
+    writeFile(wb, filename)
+
+    setIsDisabled(false);
+  }
 
   return (
     <>
-      <button className="btn-cadastrar desktop" onClick={handleShow}>
+      <button className="btn-cadastrar" onClick={handleShow}>
         Baixar Relatório
       </button>
 
@@ -25,24 +59,29 @@ function Index() {
             <div className="input-group-modal">
               <div className="input-box-modal">
                 <label>Tipo</label>
-                <select
-                  className="tipo-pesquisa"
-                >
-                  <option>Inventário</option>
-                  <option>Empréstimos</option>
+                <select className="tipo-pesquisa"
+                  value={type}
+                  onChange={(e) => setType(e.target.value)}>
+                  <option value={"inventory"}>Inventário</option>
+                  <option value={"lending"}>Empréstimos</option>
                 </select>
               </div>
-              <div className="input-box-modal">
-                <label>Formato</label>
-                <select
-                  className="tipo-pesquisa"
-                >
-                  <option>PDF</option>
-                  <option>Excel</option>
-                </select>
-              </div>
+              {type === "lending" && (
+                <div className="input-box-modal">
+                  <label>Retornados</label>
+                  <select className="tipo-pesquisa" onChange={(e) => setReturned(e.target.value)}>
+                    <option value={true}>Sim</option>
+                    <option value={false}>Não</option>
+                  </select>
+                </div>
+              )}
             </div>
           </form>
+          {isDisabled && (
+            <div className="loading-modal">
+              <Spinner animation="border" />
+            </div>
+          )}
         </Modal.Body>
         <Modal.Footer>
           <button className="btn-cancelar-modal" onClick={handleClose}>
@@ -51,6 +90,8 @@ function Index() {
           <button
             className="btn-cadastrar-modal"
             type="submit"
+            onClick={download}
+            disabled={isDisabled}
           >
             Baixar
           </button>
