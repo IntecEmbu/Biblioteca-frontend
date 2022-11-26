dbd81fadec9da0aba78ab22ec9b2146cc8c8b24c
pedrofnseca <pedropereira.pp264@gmail.com>
2022-11-26 20:36:04 -0300

    :sparkles: feat: utils de categoria

---
+++ src/utils/categoria.js
@@ -0,0 +1,123 @@
+export default function () {
+  return {
+    "Generalidades": [
+      "Generalidades",
+      "Bibliografia",
+      "Biblioteca e Informática",
+      "Enciclopédias em Geral",
+      "Nihil",
+      "Publicações em Série",
+      "Organizações e Museografia",
+      "Mídia, Jornalismo e Publicação",
+      "Coleções em Geral",
+      "Manuscritos e Livros Raros"
+    ],
+    "Filosofia e Sociologia": [
+      "Filosofia e Psicologia",
+      "Metafísica",
+      "Epistemologia, Causas, Fins, Gênero Humano",
+      "Fenômenos Paranormais",
+      "Escolas Filosóficas Específicas",
+      "Psicologia",
+      "Lógica",
+      "Ética ( Filosofia Moral)",
+      "Filosofia Antiga, Medieval e Oriental",
+      "Filosofia Moderna Ocidental"
+    ],
+    "Religião": [
+      "Religião",
+      "Teologia Natural",
+      "Bíblia",
+      "Teologia Cristã",
+      "Moral Cristã e Teologia Prática",
+      "Ordens Cristãs e Igreja Local",
+      "Teologia Social Cristã",
+      "História da Igreja Cristã",
+      "Denominações Cristãs e Seitas",
+      "Outras e Religião Comparada"
+    ],
+    "Ciências Sociais": [
+      "Ciências Sociais",
+      "Estatística Geral",
+      "Ciência Política",
+      "Economia",
+      "Direito",
+      "Administração Pública",
+      "Patologia e Serviços Sociais",
+      "Educação",
+      "Comercio, Comunicações, Transporte",
+      "Costumes, Etiqueta, Folclore"
+    ],
+    "Linguas": [
+      "Línguas",
+      "Linguística",
+      "Inglês e Inglês Antigo",
+      "Línguas Germânicas - Alemão",
+      "Língua Francesa - Romances",
+      "Língua Italiana, Romana, Retrorromana",
+      "Línguas Espanhola e Portuguesa",
+      "Línguas Itálicas, Latinas",
+      "Línguas Helênicas, Grego Clássico",
+      "Outras Línguas"
+    ],
+    "Ciências Naturais e Matemáticas": [
+      "Ciências Naturais e Matemáticas",
+      "Matemáticas",
+      "Astronomia e Ciências Afins",
+      "Física",
+      "Química e Ciências Afins",
+      "Ciências da Terra",
+      "Paleontologia, Paleozoologia",
+      "Ciências da Vida - (Biológicas)",
+      "Ciências Botânicas",
+      "Ciências Zoológicas"
+    ],
+    "Tecnologia - Ciências Aplicadas": [
+      "Tecnologia - Ciências Aplicadas",
+      "Ciências Médicas - Medicina - Saúde",
+      "Engenharia e Operações Afins",
+      "Agricultura",
+      "Economia Doméstica e Vida Familiar",
+      "Administração e Serviços Auxiliares",
+      "Engenharia Química",
+      "Manufatura",
+      "Manufatura para Fins Específicos",
+      "Construção"
+    ],
+    "Artes": [
+      "Artes",
+      "Urbanismo e Paisagismo",
+      "Arquitetura",
+      "Artes Plásticas - Escultura",
+      "Desenho e Artes Decorativas",
+      "Pintura e Pinturas",
+      "Artes Gráficas, Arte de Gravar e Gravados",
+      "Fotografia e Fotografias",
+      "Música",
+      "Artes Recreativas e de Representar"
+    ],
+    "Literatura e Retórica": [
+      "Literatura e Retórica",
+      "Literatura Americana em Inglês",
+      "Literaturas Inglesa e Inglesa Antiga",
+      "Literaturas de Línguas Germânicas",
+      "Literatura de Línguas Romance",
+      "Literaturas Italiana, Romana, Retrorromana",
+      "Literaturas Espanholas e Portuguesas",
+      "Literaturas Itálicas - Literatura Latina",
+      "Literaturas Helênicas - Grega Clássica"
+    ],
+      "Geografia, História e Biografia": [
+      "Geografia, História e Biografia",
+      "Geografias e Viagens",
+      "Biografia, Genealogia, Insígnia-(Emblema)",
+      "História do Mundo Antigo",
+      "História Geral da Europa",
+      "História Geral da Ásia e Extremo Oriente",
+      "História Geral da África",
+      "História Geral da América do Norte",
+      "História Geral da América do Sul",
+      "História Geral de Outras Regiões"
+    ]
+  }
+}
